bba1c21700fb90c7158f9c077feb13fc2090e2a9
Xavier Lepaul <xavier@lepaul.fr>
2018-02-24 15:43:13 +0100

    single gallery for all images

--- footer.php
+++ footer.php
@@ -1,2 +1,2 @@
 <footer>© 2016-<?php echo date('Y'); ?> <?php echo get_bloginfo('name'); ?> | <a href="mailto:info@bellevuefarm.se">info@bellevuefarm.se</a></footer>
-<script>baguetteBox.run('.gallery');</script>
+<script>baguetteBox.run('main');</script>
